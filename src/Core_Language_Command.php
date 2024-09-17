df1b888c3db057d01945abe3bea4e76d378ae9f6
Ryan Neudorf <ryan.neudorf@gmail.com>
2024-09-17 14:51:17 -0700

    Added output formats.

--- src/Core_Language_Command.php
+++ src/Core_Language_Command.php
@@ -178,6 +178,18 @@ class Core_Language_Command extends WP_CLI\CommandWithTranslation {
 	 * [--activate]
 	 * : If set, the language will be activated immediately after install.
 	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format. Used when installing languages for all plugins.
+	 * ---
+	 * default: summary
+	 * options:
+	 *   - table
+	 *   - csv
+	 *   - json
+	 *   - summary
+	 * ---
+	 *
+	 *
 	 * ## EXAMPLES
 	 *
 	 *     # Install the Brazilian Portuguese language.
@@ -200,6 +212,19 @@ class Core_Language_Command extends WP_CLI\CommandWithTranslation {
 			WP_CLI::error( 'Only a single language can be active.' );
 		}
 
+		// set default format: summary
+		if ( empty( $assoc_args['format'] ) ) {
+			$assoc_args['format'] = 'summary';
+		}
+
+		// suppress logging when format json or csv
+		if ( in_array( $assoc_args['format'], array( 'json', 'csv' ), true ) ) {
+			$logger = new \WP_CLI\Loggers\Quiet();
+			\WP_CLI::set_logger( $logger );
+		}
+
+		$result = array();
+
 		$available = $this->get_installed_languages();
 
 		$successes = 0;
@@ -207,8 +232,13 @@ class Core_Language_Command extends WP_CLI\CommandWithTranslation {
 		$skips     = 0;
 		foreach ( $language_codes as $language_code ) {
 
+			$result = [
+				'locale' => $language_code,
+			];
+
 			if ( in_array( $language_code, $available, true ) ) {
 				\WP_CLI::log( "Language '{$language_code}' already installed." );
+				$result['status'] = 'already installed';
 				++$skips;
 			} else {
 				$response = $this->download_language_pack( $language_code );
@@ -219,12 +249,15 @@ class Core_Language_Command extends WP_CLI\CommandWithTranslation {
 
 					// Skip if translation is not yet available.
 					if ( 'not_found' === $response->get_error_code() ) {
+						$result['status'] = 'not available';
 						++$skips;
 					} else {
+						$result['status'] = 'not installed';
 						++$errors;
 					}
 				} else {
 					\WP_CLI::log( "Language '{$language_code}' installed." );
+					$result['status'] = 'installed';
 					++$successes;
 				}
 			}
@@ -232,6 +265,13 @@ class Core_Language_Command extends WP_CLI\CommandWithTranslation {
 			if ( WP_CLI\Utils\get_flag_value( $assoc_args, 'activate' ) ) {
 				$this->activate_language( $language_code );
 			}
+
+			$results[] = (object) $result;
+
+		}
+
+		if ( 'summary' !== $assoc_args['format'] ) {
+			\WP_CLI\Utils\format_items( $assoc_args['format'], $results, array( 'locale', 'status',  ) );
 		}
 
 		\WP_CLI\Utils\report_batch_operation_results( 'language', 'install', $count, $successes, $errors, $skips );
